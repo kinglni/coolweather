91dd28d452a03291a4800c6dfa8c98eb261d20ea
kinglni <kinglni@163.com>
2017-07-19 13:43:46 +0800

    choose area finish

---
+++ src/com/coolweather/app/activity/WeatherActivity.java
@@ -0,0 +1,17 @@
+package com.coolweather.app.activity;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+
+public class WeatherActivity extends Activity implements OnClickListener{
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
